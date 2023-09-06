80f256b9943b9cde5ffbf56919aa16cd902ce998
wisamaisam <wisamaisam@gmail.com>
2023-09-06 20:10:14 +0700

    Add product object for offers related task

---
+++ Object Repository/Offers/Product/fieldGambarJual.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>fieldGambarJual</name>
+   <tag></tag>
+   <elementGuidId>4a3e05e0-9eae-4966-bc6f-4b26665224e1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[@class='form-image rounded-4 img-preview']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='form-image rounded-4 img-preview']</value>
+      <webElementGuid>7aee0904-dcda-4d91-a275-e3bc67c33608</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
