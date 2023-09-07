997ac76ff710fe2be51b1745b49e4f76fb68339d
wisamaisam <wisamaisam@gmail.com>
2023-09-07 14:56:01 +0700

    Test Suite template

---
+++ Test Suites/Offers/Seller respond to Offer.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Seller respond to Offer</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>61042022-acb2-47ac-8f5d-3e62b6ef4988</testSuiteGuid>
+</TestSuiteEntity>
